db749e7387f0b2a5bdeb47a2d68c77b4ea9d8944
Jacob Humston <jhumston24@sciototech.org>
2023-08-25 14:28:39 -0400

    added an http func

--- src/functions/http.js
+++ src/functions/http.js
@@ -6,3 +6,21 @@
 // Imports
 import * as http from 'http';
 import * as validate from './validator.js';
+
+/**
+ * Create a quick http server that responses with plain text.
+ * @param {number} port Port to listen to.
+ * @param {string} message Message to send.
+ * @returns {http.Server} The created server.
+ */
+export function quickServer(port, message) {
+    validate.param('number', port, 'port');
+    validate.param('string', message, 'message');
+    return http
+        .createServer(function (_, response) {
+            response.writeHead(200, { 'Content-Type': 'text/plain' });
+            response.write(message);
+            response.end();
+        })
+        .listen(port);
+}
--- test/run.js
+++ test/run.js
@@ -2,3 +2,5 @@
 import * as lib from '../src/core.js';
 
 // Tests
+const server = lib.quickServer(1002, 'pong');
+lib.wait(2000).then(() => server.close());
