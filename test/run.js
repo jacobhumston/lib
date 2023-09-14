077efb339f84703d5175510aa6ec0477785d5a22
Jacob Humston <jhumston24@sciototech.org>
2023-09-14 13:56:46 -0400

    Changed test file.

--- test/run.js
+++ test/run.js
@@ -2,8 +2,4 @@
 import * as lib from '../src/core.js';
 
 // Tests
-lib.log("Error", "Something went VERY wrong!", false)
-lib.log("Debug", "Something went VERY wrong!", false)
-lib.log("Log", "Something went VERY wrong!", false)
-lib.log("Success", "Something went VERY wrong!", false)
-lib.log("Warn", "Something went VERY wrong!", false)
+const cache = new lib.Cache(1);
