0d1fcea5a8a8ed869b2c0dc1e600ac9db004ad82
Jacob Humston <jhumston24@sciototech.org>
2023-09-14 11:50:44 -0400

    Added cache to core

--- src/core.js
+++ src/core.js
@@ -5,6 +5,7 @@ import { param, paramObject } from './functions/validator.js';
 import { quickServer, quickRedirectServer } from './functions/http.js';
 import { TimeConversion } from './classes/timeConversion.js';
 import { getColor, log } from './functions/console.js';
+import { Cache } from './classes/cache.js';
 
 // Exports
 export {
@@ -19,4 +20,5 @@ export {
     TimeConversion,
     getColor,
     log,
+    Cache,
 };
