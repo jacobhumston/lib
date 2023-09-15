9908281ff73621736da47b1b679d3b26ced34827
Jacob Humston's School Account <113031145+jacobhumston-school@users.noreply.github.com>
2023-09-15 14:17:20 +0000

    edit test

--- test/run.js
+++ test/run.js
@@ -3,5 +3,8 @@ import * as lib from '../src/core.js';
 
 // Tests
 
-const time = new lib.TimeConversion({ minutes: .5 });
-lib.log("Success", JSON.stringify(time.roundedConvert()))
+const time = new lib.TimeConversion({ minutes: 1321, months: 400, years: 2134 });
+for (const name in time.roundedConvert()) {
+    const value = time.roundedConvert()[name];
+    lib.log('Debug', `${name}: ${value}`);
+}
