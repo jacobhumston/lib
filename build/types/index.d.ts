9b99e1c9f0d4e65bf9e145a8711ebeeff85a0de6
jacobhumston <humstonjacob@gmail.com>
2025-03-15 10:37:04 -0400

    Starting from scratch!

--- build/types/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { Logger } from './classes/logger.js';
-export * as consoleColors from './data/consoleColors.js';
-//# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-export { Logger } from './classes/logger.js';
-export * as consoleColors from './data/consoleColors.js';
